223d9aedfd35f033042be8f0220327b8472fc744
Giriprasath S <giriprasathsks@gmail.com>
2025-09-06 13:50:16 +0530

    for production ready

--- meditech-website/src/index.js
+++ meditech-website/src/index.js
@@ -4,6 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Disable React DevTools message
+if (process.env.REACT_APP_DISABLE_DEVTOOLS === 'true') {
+  // Suppress React DevTools download message
+  window.__REACT_DEVTOOLS_GLOBAL_HOOK__ = {
+    isDisabled: true,
+    supportsFiber: true,
+    inject: () => {},
+    onCommitFiberRoot: () => {},
+    onCommitFiberUnmount: () => {},
+  };
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
